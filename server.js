a7113d52a4b42a4a14adf440da4e70d2be863372
snehagupta9582883065 <snehagupta706588@gmail.com>
2026-01-29 12:55:15 +0530

    cross origibn access

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require("./src/models");
 const app = express();
 
 var corsOptions = {
-    origin: ["http://localhost:8081", "http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:8081", "http://127.0.0.1:3000", "http://127.0.0.1:5173"]
+    origin: "*"
 };
 
 app.use(cors(corsOptions));
